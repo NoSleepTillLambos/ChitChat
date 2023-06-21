1cc8302d43492505260e43afd2a3dac4d879082c
Liam Wedge <21100218@virtualwindow.co.za>
2023-06-21 21:40:18 +0200

    last commit

--- app/src/main/java/com/example/chitchat/screens/ProfileScreen.kt
+++ app/src/main/java/com/example/chitchat/screens/ProfileScreen.kt
@@ -17,9 +17,12 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
@@ -29,6 +32,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
@@ -126,7 +130,9 @@ fun ProfileContent(
         Row(modifier = Modifier
             .fillMaxWidth()
             .padding(5.dp)) {
-            Text(text = "Your name", modifier = Modifier.width(100.dp).padding(6.dp))
+            Text(text = "Your name", modifier = Modifier
+                .width(100.dp)
+                .padding(6.dp))
             TextField(
                 value = name,
                 onValueChange = onNameChange,
@@ -159,10 +165,18 @@ fun ProfileContent(
             // calling the logout function and sending us to login screen
             Button(onClick = { onLogout.invoke()
             }) {
+                Icon(imageVector = Icons.Default.Close, contentDescription = null)
                 Text(text = "Logout", color = Color.White)
             }
             
         }
+
+        SectionDivider()
+
+        Text(text = "Copyright ChitChat 2023",
+            modifier = Modifier.padding(10.dp)
+                .align(CenterHorizontally))
+
     }
 
 }
